7da24c00a6a10fc4e6281481a3b65017c0888bcc
caoyu <1755475455@qq.com>
2023-12-03 22:02:32 +0800

    增加了数据库，并且实现了chunks级别的增量更新

--- pages/配置.py
+++ pages/配置.py
@@ -2,6 +2,34 @@ import streamlit as st
 import os
 from utils import init_session_state, save_data, find_all_files
 from configs.config import USER_CONFIG_PATH
+from langchain.document_loaders import UnstructuredMarkdownLoader, PyMuPDFLoader, UnstructuredFileLoader
+from langchain.indexes import SQLRecordManager, index
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+def save_to_vector_db(file_paths):
+    docs = []
+    loader_dict = {
+        'md': UnstructuredMarkdownLoader,
+        'pdf': PyMuPDFLoader,
+        'txt': UnstructuredFileLoader
+    }
+    for file_path in file_paths:
+        loader = loader_dict[file_path.split('.')[-1]](file_path)
+        docs.extend(loader.load())
+
+    # 切分文档
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=150)
+    split_docs = text_splitter.split_documents(docs)
+
+    result = index(
+        split_docs,
+        st.session_state.record_manager,
+        st.session_state.vector_db,
+        cleanup="incremental",
+        source_id_key="source",
+    )
+    return result
 
 
 def main():
@@ -49,9 +77,28 @@ def main():
             file_paths = []
             for knowledge_dir in knowledge_dirs:
                 file_paths.extend(find_all_files(knowledge_dir, target_file_types))
-            st.write(len(file_paths))
+            # 将目录下的文件存入向量数据库
+            if 'vector_db' in st.session_state:
+                message = st.empty()
+                message.text("正在构建本地知识库，可能会需要一点时间...")
+                save_db_res = save_to_vector_db(file_paths)
+                message.text("增加了{}个chunks, 更新了{}个chunks, 跳过了{}个chunks, 删除了{}个chunks".format(
+                    save_db_res['num_added'],
+                    save_db_res['num_updated'],
+                    save_db_res['num_skipped'],
+                    save_db_res['num_deleted']
+                ))
 
-    save_data(st.session_state.to_dict(), USER_CONFIG_PATH)
+            else:
+                st.write('没有向量数据库')
+
+    user_config = {
+        'embedding_api_key': st.session_state['embedding_api_key'],
+        'llm_api_key': st.session_state['llm_api_key'],
+        'knowledge_paths': st.session_state['knowledge_paths'],
+        'file_types': st.session_state['file_types']
+    }
+    save_data(user_config, USER_CONFIG_PATH)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,12 +1,16 @@
 import json
 import os
 import streamlit as st
-from configs.config import USER_CONFIG_PATH
+from configs.config import USER_CONFIG_PATH, VECTOR_DB_PATH, INDEX_DB_URL
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import SQLRecordManager, index
+
 
 
 def save_data(data, file_path):
     with open(file_path, 'w') as f:
-        json.dump(data, f, indent=2)
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 
 # 从 JSON 文件加载数据
@@ -27,7 +31,19 @@ def find_all_files(path, target_file_types):
 
 
 def init_session_state(state_path=USER_CONFIG_PATH):
+    # 用户配置的api_key，知识库路径等
     session_state = load_data(state_path)
     for k, v in session_state.items():
         if k not in st.session_state:
             st.session_state[k] = v
+    # 配置向量数据库
+    if 'embedding_api_key' in st.session_state:
+        embedding = OpenAIEmbeddings(openai_api_key=st.session_state.embedding_api_key)
+        if 'vector_db' not in st.session_state:
+            st.session_state.vector_db = Chroma(persist_directory=VECTOR_DB_PATH, embedding_function=embedding)
+    # 配置index数据库
+    if 'record_manager' not in session_state:
+        st.session_state.record_manager = SQLRecordManager('chroma_index', db_url=INDEX_DB_URL)
+        st.session_state.record_manager.create_schema()
+
+        
