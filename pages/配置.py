49a8292fc32b90244f10d86425c6658ce6163524
caoyu <1755475455@qq.com>
2023-12-06 21:34:50 +0800

    实现了对话,但是存在会重复问题的情况

--- pages/配置.py
+++ pages/配置.py
@@ -2,25 +2,30 @@ import streamlit as st
 import os
 from utils import init_session_state, save_data, find_all_files
 from configs.config import USER_CONFIG_PATH
-from langchain.document_loaders import UnstructuredMarkdownLoader, PyMuPDFLoader, UnstructuredFileLoader
-from langchain.indexes import SQLRecordManager, index
+from langchain.document_loaders import (
+    UnstructuredMarkdownLoader,
+    PyMuPDFLoader,
+    UnstructuredFileLoader,
+)
+from langchain.indexes import index
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 def batch_save_to_vector_db(file_paths):
     docs = []
     loader_dict = {
-        'md': UnstructuredMarkdownLoader,
-        'pdf': PyMuPDFLoader,
-        'txt': UnstructuredFileLoader
+        "md": UnstructuredMarkdownLoader,
+        "pdf": PyMuPDFLoader,
+        "txt": UnstructuredFileLoader,
     }
     # 加载文件
     error_files = []
     for file_path in file_paths:
         try:
-            loader = loader_dict[file_path.split('.')[-1]](file_path)
+            loader = loader_dict[file_path.split(".")[-1]](file_path)
             docs.extend(loader.load())
-        except:
+        except Exception as e:
+            st.write(e)
             error_files.append(file_path)
 
     # 切分文档
@@ -34,81 +39,104 @@ def batch_save_to_vector_db(file_paths):
         cleanup="incremental",
         source_id_key="source",
     )
-    return result
+    return result, error_files
 
 
 def save_to_vector_db(file_paths, batch_size=20):
+    process_bar = st.progress(0, text="正在处理第0个文件")
     if len(file_paths) > batch_size:
+        result = {"num_added": 0, "num_updated": 0, "num_skipped": 0, "num_deleted": 0}
+        error_files = []
         for i in range(0, len(file_paths), batch_size):
-            batch_save_to_vector_db(file_paths[i:i + batch_size])
+            # streamlit进度条显示
+            process_bar.progress(i / len(file_paths), text="正在处理第{}个文件".format(i))
+            tmp_res, tmp_err = batch_save_to_vector_db(file_paths[i : i + batch_size])
+            result["num_added"] += tmp_res["num_added"]
+            result["num_updated"] += tmp_res["num_updated"]
+            result["num_skipped"] += tmp_res["num_skipped"]
+            result["num_deleted"] += tmp_res["num_deleted"]
+            error_files.extend(tmp_err)
     else:
-        batch_save_to_vector_db(file_paths)
+        result, error_files = batch_save_to_vector_db(file_paths)
+    process_bar.progress(1.0, text="处理完成")
+    return result, error_files
 
 
 def main():
     init_session_state()
-    st.title('配置')
+    st.title("配置")
 
     st.divider()
 
-    st.header('Embedding模型')
-    st.text_input(label='embedding_api_key',
-                  placeholder='请输入用于embedding的Api Key，目前只能OpenAI的API',
-                  label_visibility='hidden',
-                  key='embedding_api_key')
-
-    st.header('LLM模型')
-    st.text_input(label='llm_api_key',
-                  placeholder='请输入LLM的Api Key，目前只能OpenAI的API',
-                  label_visibility='hidden',
-                  key='llm_api_key')
-
-    st.divider()
-
-    st.header('本地知识库路径')
-    st.text_area(label='knowledge_paths',
-                 placeholder='请输入文件夹路径，每个路径一行，用回车进行分割',
-                 label_visibility='hidden',
-                 key='knowledge_paths')
-
-    st.header('知识库中的文件类型')
-    st.multiselect(
-        '知识库文件类型',
-        placeholder='请从下拉菜单中选择文件类型',
-        key="file_types",
-        label_visibility='hidden',
-        options=['txt', 'md', 'pdf']
+    st.header("Embedding模型")
+    st.text_input(
+        label="embedding_api_key",
+        placeholder="请输入用于embedding的Api Key，目前只能OpenAI的API",
+        label_visibility="hidden",
+        key="embedding_api_key",
     )
 
-    if st.button('构建知识库'):
+    st.header("LLM模型")
+    st.text_input(
+        label="llm_api_key",
+        placeholder="请输入LLM的Api Key，目前只能OpenAI的API",
+        label_visibility="hidden",
+        key="llm_api_key",
+    )
+
+    st.divider()
+
+    st.header("本地知识库路径")
+    st.text_area(
+        label="knowledge_paths",
+        placeholder="请输入文件夹路径，每个路径一行，用回车进行分割",
+        label_visibility="hidden",
+        key="knowledge_paths",
+    )
+
+    st.header("知识库中的文件类型")
+    st.multiselect(
+        "知识库文件类型",
+        placeholder="请从下拉菜单中选择文件类型",
+        key="file_types",
+        label_visibility="hidden",
+        options=["txt", "md", "pdf"],
+    )
+
+    if st.button("构建知识库"):
         if len(st.session_state.knowledge_paths) == 0:
-            st.warning('请先输入知识库路径')
+            st.warning("请先输入知识库路径")
         else:
             # 获得目录下的文件
-            knowledge_dirs = st.session_state.knowledge_paths.split('\n')
+            knowledge_dirs = st.session_state.knowledge_paths.split("\n")
             target_file_types = set(st.session_state.file_types)
             file_paths = []
             for knowledge_dir in knowledge_dirs:
                 file_paths.extend(find_all_files(knowledge_dir, target_file_types))
             # 将目录下的文件存入向量数据库
-            if 'vector_db' in st.session_state:
-                st.write('一共有{}个文件需要处理'.format(len(file_paths)))
-                save_db_res = save_to_vector_db(file_paths)
-                st.write("增加了{}个chunks, 更新了{}个chunks, 跳过了{}个chunks, 删除了{}个chunks".format(
-                    save_db_res['num_added'],
-                    save_db_res['num_updated'],
-                    save_db_res['num_skipped'],
-                    save_db_res['num_deleted']
-                ))
+            if "vector_db" in st.session_state:
+                st.write("一共有{}个文件需要处理".format(len(file_paths)))
+                save_db_res, save_db_err = save_to_vector_db(file_paths)
+                st.write(
+                    "增加了{}个chunks, 更新了{}个chunks, 跳过了{}个chunks, 删除了{}个chunks".format(
+                        save_db_res["num_added"],
+                        save_db_res["num_updated"],
+                        save_db_res["num_skipped"],
+                        save_db_res["num_deleted"],
+                    )
+                )
+                if len(save_db_err) > 0:
+                    st.write("处理失败的文件：")
+                    st.write(save_db_err)
 
             else:
-                st.write('没有向量数据库')
+                st.write("没有向量数据库")
 
     user_config = {
-        'embedding_api_key': st.session_state['embedding_api_key'],
-        'llm_api_key': st.session_state['llm_api_key'],
-        'knowledge_paths': st.session_state['knowledge_paths'],
-        'file_types': st.session_state['file_types']
+        "embedding_api_key": st.session_state["embedding_api_key"],
+        "llm_api_key": st.session_state["llm_api_key"],
+        "knowledge_paths": st.session_state["knowledge_paths"],
+        "file_types": st.session_state["file_types"],
     }
     save_data(user_config, USER_CONFIG_PATH)
 
--- 知识库问答.py
+++ 知识库问答.py
@@ -1,13 +1,15 @@
 import streamlit as st
 from utils import init_session_state, StreamingResponseCallbackHandler
 from langchain.chains import ConversationalRetrievalChain
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.chains.llm import LLMChain
 from langchain.chains.conversational_retrieval.prompts import (
     CONDENSE_QUESTION_PROMPT,
     QA_PROMPT,
 )
 from langchain.chains.question_answering import load_qa_chain
+
+# 参考实现: https://python.langchain.com.cn/docs/use_cases/question_answering/how_to/chat_vector_db
 
 
 def main():
@@ -42,35 +44,29 @@ def main():
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
 
-        llm = OpenAI(
+        llm = ChatOpenAI(
             openai_api_key=st.session_state.llm_api_key,
             temperature=0,
-            model="gpt-3.5-turbo-instruct",
-        )
-
-        # 关键在于要重写这个callback
-        streaming_llm = OpenAI(
-            openai_api_key=st.session_state.llm_api_key,
+            model="gpt-3.5-turbo",
             streaming=True,
             callbacks=[StreamingResponseCallbackHandler(message_placeholder)],
-            temperature=0,
-            model="gpt-3.5-turbo-instruct",
         )
-
-        question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
-        doc_chain = load_qa_chain(streaming_llm, chain_type="stuff", prompt=QA_PROMPT)
-
-        qa = ConversationalRetrievalChain(
-            retriever=st.session_state.vector_db.as_retriever(),
-            combine_docs_chain=doc_chain,
-            question_generator=question_generator,
+        qa = ConversationalRetrievalChain.from_llm(
+            llm,
+            st.session_state.vector_db.as_retriever(),
+            return_source_documents=True,
+            chain_type="stuff",
         )
-
         result = qa(
-            {"question": prompt, "chat_history": st.session_state.knowledge_messages}
+            {
+                "question": "请回答下面的问题, 并且结果应该尽可能地详细和全面, 并且回答的格式应当正确且易读, 回答的语言应当和提问的语言相同:"
+                + prompt,
+                "chat_history": st.session_state.knowledge_messages[1:],
+            }
         )
+        print(result["source_documents"][0])
+        print(result["answer"])
         st.session_state.knowledge_messages.append((prompt, result["answer"]))
-        print(st.session_state.knowledge_messages)
 
 
 if __name__ == "__main__":
